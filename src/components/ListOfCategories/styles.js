a1fff54681aa28ae6cfa90718364f3e6a10f7699
alexsk88 <titoantifa69@gmail.com>
2022-03-10 12:57:51 -0500

    [add] refactoring

--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,12 +1,30 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { slideInDown } from '../../styles/animation'
 
 export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
-  &::-webkit-scrollbar {
-    display: none;
-  }
+  ${props =>
+    props.fixed &&
+    css`
+       {
+        background: #fff;
+        border-radius: 60px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+        left: 0;
+        margin: 0 auto;
+        max-width: 400px;
+        padding: 5px;
+        position: fixed;
+        right: 0;
+        top: 4px;
+        /* transform: scale(0.5); */
+        z-index: 1;
+      }
+    `}
+
+  ${slideInDown({ time: '.2s', type: 'ease' })}
 `
 
 export const Item = styled.li`
--- src/styles/animation.js
+++ src/styles/animation.js
@@ -10,7 +10,38 @@ const fadeInKeyFrames = keyframes`
     opacity: 1;
   }
 `
-
 export const fadeIn = ({ time = '1s', type = 'ease' } = {}) => css`
   animation: ${time} ${fadeInKeyFrames} ${type};
+`
+
+const slideInLeftKeyFrames = keyframes`
+  0% {
+    -webkit-transform: translate3d(-100%, 0, 0);
+    transform: translate3d(-100%, 0, 0);
+    visibility: visible;
+  }
+  to {
+    -webkit-transform: translateZ(0);
+    transform: translateZ(0);
+  }
+`
+
+export const slideInLeft = ({ time = '1s', type = 'ease' } = {}) => css`
+  animation: ${time} ${slideInLeftKeyFrames} ${type};
+`
+
+const slideInDownKeyFrames = keyframes`
+  0% {
+    -webkit-transform: translate3d(0, -100%, 0);
+    transform: translate3d(0, -100%, 0);
+    visibility: visible;
+    transform: scale(0.5);
+  }
+  to {
+    -webkit-transform: translateZ(0);
+    transform: translateZ(0);
+  }
+`
+export const slideInDown = ({ time = '1s', type = 'ease' } = {}) => css`
+  animation: ${time} ${slideInDownKeyFrames} ${type};
 `
