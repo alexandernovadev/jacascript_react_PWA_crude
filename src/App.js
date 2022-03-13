2d5fa432b2213c8958eb5a3ff142f0349acf653d
alexsk88 <titoantifa69@gmail.com>
2022-03-13 00:07:03 -0500

    [add] hook to mutate

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { GlobalStyle } from './styles/GlobalStyles'
 
-export const App = () => (
-  <>
-    <GlobalStyle />
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <Logo />
-    </div>
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={1} />
-  </>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+
+  return (
+    <>
+      <GlobalStyle />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Logo />
+      </div>
+      {detailId ? (
+        <PhotoCardWithQuery id={detailId} />
+      ) : (
+        <>
+          <ListOfCategories />
+          <ListOfPhotoCards categoryId={1} />
+        </>
+      )}
+    </>
+  )
+}
